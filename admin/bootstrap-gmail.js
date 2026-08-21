9a91035914204565b24cbcfd226de84dc78a8adb
shayanakhund-droid <shayan.akhund@activhauling.com>
2026-08-21 13:56:30 -0400

    Load Gmail workspace module

--- admin/bootstrap-gmail.js
+++ admin/bootstrap-gmail.js
@@ -1,8 +1,10 @@
 (function(){
   if(window.__mightGmailLoaded) return;
   window.__mightGmailLoaded=true;
-  const script=document.createElement('script');
-  script.src='gmail-loader.js?v=1';
-  script.async=false;
-  document.head.appendChild(script);
+  ['gmail-loader.js?v=2','gmail-workspace.js?v=1'].forEach((src)=>{
+    const script=document.createElement('script');
+    script.src=src;
+    script.async=false;
+    document.head.appendChild(script);
+  });
 })();
